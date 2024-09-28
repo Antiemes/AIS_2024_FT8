a57ab23244e59818bb4dfdb3ed189ce22b0d57a8
Antiemes <vg@electrit.hu>
2024-09-28 21:24:57 +0200

    QRA from CQ type messages

--- proc/proc.py
+++ proc/proc.py
@@ -2,18 +2,31 @@
 
 from datetime import datetime
 import gzip
+import re
 
 all_file = gzip.open('ALL.TXT.gz', 'rt')
 
 while True:
-    row = all_file.readline()
+    row = all_file.readline().strip()
     if not row:
         break
+    msg_time = datetime.strptime(row[0:13] + " UTC", "%y%m%d_%H%M%S %Z")
     msg_freq = row[17:23]
+    msg_direction = row[24:26]
+    msg_mode = row[27:30]
+    msg_text = row[48:]
+    #print(msg_text + "k")
     if not msg_freq == "14.074":
         continue
-    msg_time = datetime.strptime(row[0:13] + " UTC", "%y%m%d_%H%M%S %Z")
-    print(msg_time)
+    if not msg_direction == "Rx":
+        continue
+    if not msg_mode == "FT8":
+        continue
+    if m := re.match('CQ ([A-Z0-9/]+) ([A-Z]{2}[0-9]{2})', msg_text):
+        qra = m.group(2)
+        print(qra)
+    else:
+        pass
+    #print(msg_time)
 
 all_file.close()
-
